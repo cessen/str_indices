10bf1d68f0c891891d153eaa3bdb34c8db424c80
Nathan Vegdahl <cessen@cessen.com>
2022-03-14 10:41:26 -0700

    Make `chars::to_byte_idx()` faster and more comprehensible.

--- src/chars.rs
+++ src/chars.rs
@@ -54,45 +54,57 @@ fn to_byte_idx_inner<T: ByteChunk>(text: &str, char_idx: usize) -> usize {
     let mut char_count = 0;
 
     // Take care of any unaligned bytes at the beginning.
-    let mut i = 0;
-    while i < start.len() && char_count <= char_idx {
-        char_count += ((start[i] & 0xC0) != 0x80) as usize;
-        i += 1;
-    }
-    byte_count += i;
-
-    // Use chunks to count multiple bytes at once.
-    let mut i = 0;
-    let mut acc = T::zero();
-    let mut acc_i = 0;
-    while i < middle.len() && (char_count + (T::SIZE * (acc_i + 1))) <= char_idx {
-        acc = acc.add(middle[i].bitand(T::splat(0xc0)).cmp_eq_byte(0x80));
-        acc_i += 1;
-        if acc_i == T::MAX_ACC || (char_count + (T::SIZE * (acc_i + 1))) >= char_idx {
-            char_count += (T::SIZE * acc_i) - acc.sum_bytes();
-            acc_i = 0;
-            acc = T::zero();
+    for byte in start.iter() {
+        char_count += ((*byte & 0xC0) != 0x80) as usize;
+        if char_count > char_idx {
+            break;
         }
-        i += 1;
+        byte_count += 1;
     }
-    char_count += (T::SIZE * acc_i) - acc.sum_bytes();
-    byte_count += i * T::SIZE;
+
+    // Process chunks in the fast path.
+    let mut chunks = middle;
+    let mut max_round_len = char_idx.saturating_sub(char_count) / T::MAX_ACC;
+    while max_round_len > 0 && !chunks.is_empty() {
+        // Choose the largest number of chunks we can do this round
+        // that will neither overflow `max_acc` nor blast past the
+        // remaining line breaks we're looking for.
+        let round_len = T::MAX_ACC.min(max_round_len).min(chunks.len());
+        max_round_len -= round_len;
+        let round = &chunks[..round_len];
+        chunks = &chunks[round_len..];
+
+        // Process the chunks in this round.
+        let mut acc = T::zero();
+        for chunk in round.iter() {
+            acc = acc.add(chunk.bitand(T::splat(0xc0)).cmp_eq_byte(0x80));
+        }
+        char_count += (T::SIZE * round_len) - acc.sum_bytes();
+        byte_count += T::SIZE * round_len;
+    }
+
+    // Process chunks in the slow path.
+    for chunk in chunks.iter() {
+        let new_char_count =
+            char_count + T::SIZE - chunk.bitand(T::splat(0xc0)).cmp_eq_byte(0x80).sum_bytes();
+        if new_char_count >= char_idx {
+            break;
+        }
+        char_count = new_char_count;
+        byte_count += T::SIZE;
+    }
 
     // Take care of any unaligned bytes at the end.
     let end = &text.as_bytes()[byte_count..];
-    let mut i = 0;
-    while i < end.len() && char_count <= char_idx {
-        char_count += ((end[i] & 0xC0) != 0x80) as usize;
-        i += 1;
+    for byte in end.iter() {
+        char_count += ((*byte & 0xC0) != 0x80) as usize;
+        if char_count > char_idx {
+            break;
+        }
+        byte_count += 1;
     }
-    byte_count += i;
 
-    // Finish up
-    if byte_count == text.len() && char_count <= char_idx {
-        byte_count
-    } else {
-        byte_count - 1
-    }
+    byte_count
 }
 
 #[inline(always)]
